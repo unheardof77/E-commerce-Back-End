5341092af9eb9a4f4466be00758868a3eba9de9f
Morgan Tolman <morgan.tolman04@gmail.com>
2022-10-21 18:24:35 -0700

    Finished product routes.  Safe revert point.

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -16,9 +16,18 @@ router.get('/', async (req, res) => {
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findByPk(req.params.id, {include:[{model: Category}, {model: Tag}]});
+
+    if(!productData) return res.status(404).json({message:"No product found with that id."});
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  };
 });
 
 // create new product
@@ -95,8 +104,15 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try{
+    const deletedProduct = await Product.destroy({where:{id:req.params.id}});
+    if(deletedProduct == 0) return res.status(404).json({message:"No product found with that id."});
+    res.status(200).json(deletedProduct);
+  }catch(err){
+    res.status(500).json(err);
+  };
 });
 
 module.exports = router;
